ca78a3b5e167b37edf268852573cf64de738615b
Nilesh <5434666+TheNilesh@users.noreply.github.com>
2026-08-05 09:20:31 +0530

    fix fdfind, karabiner config for wireless kb

--- dot_config/fish/conf.d/abbr.fish
+++ dot_config/fish/conf.d/abbr.fish
@@ -7,7 +7,9 @@ abbr -a t     'tmux new -A -s main'
 abbr -a tree  'exa -T'
 abbr -a tree2 'exa -T --level 2'
 abbr -a tree3 'exa -T --level 3'
-abbr -a fd    fdfind
+if command -q fdfind
+    abbr -a fd fdfind
+end
 
 # decode secret from k get secret -o json
 abbr -a secret-decode "jq '.data | map_values(@base64d)'"
